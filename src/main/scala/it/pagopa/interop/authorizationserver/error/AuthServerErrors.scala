56b21d9ce5449c41f3704af86cd481dfd89ab185
galales <25105748+galales@users.noreply.github.com>
2022-04-05 16:39:07 +0200

    pin-1346: Fix errors numbers

--- src/main/scala/it/pagopa/interop/authorizationserver/error/AuthServerErrors.scala
+++ src/main/scala/it/pagopa/interop/authorizationserver/error/AuthServerErrors.scala
@@ -10,25 +10,25 @@ object AuthServerErrors {
   final case object InternalServerError extends ComponentError("0002", "There was an internal server error")
 
   final case class PurposeNotFound(clientId: UUID, purposeId: UUID)
-      extends ComponentError("0009", s"Purpose $purposeId not found for client $clientId")
+      extends ComponentError("0003", s"Purpose $purposeId not found for client $clientId")
 
   final case class InactiveClient(clientId: UUID, errorMessages: List[String])
-      extends ComponentError("0010", s"Client $clientId is inactive: ${errorMessages.mkString(", ")}")
+      extends ComponentError("0004", s"Client $clientId is inactive: ${errorMessages.mkString(", ")}")
 
   final case class CreateTokenRequestError(error: String)
-      extends ComponentError("0011", s"Error while creating a token for this request - $error")
+      extends ComponentError("0005", s"Error while creating a token for this request - $error")
 
   final case class MissingActivePurposeVersion(purposeId: UUID)
-      extends ComponentError("0012", s"There is no active version for purpose $purposeId")
+      extends ComponentError("0006", s"There is no active version for purpose $purposeId")
 
   final case class MissingActivePurposesVersions(purposesIds: Seq[UUID])
-      extends ComponentError("0013", s"There is no active version for purposes ${purposesIds.mkString(", ")}")
+      extends ComponentError("0007", s"There is no active version for purposes ${purposesIds.mkString(", ")}")
 
   final case object PurposeIdNotProvided
-      extends ComponentError("0017", "purposeId claim does not exist in this assertion")
+      extends ComponentError("0008", "purposeId claim does not exist in this assertion")
 
-  final case class InactivePurpose(state: String)   extends ComponentError("0014", s"Purpose is in state $state")
-  final case class InactiveEservice(state: String)  extends ComponentError("0015", s"E-Service is in state $state")
-  final case class InactiveAgreement(state: String) extends ComponentError("0016", s"Agreement is in state $state")
+  final case class InactivePurpose(state: String)   extends ComponentError("0009", s"Purpose is in state $state")
+  final case class InactiveEservice(state: String)  extends ComponentError("0010", s"E-Service is in state $state")
+  final case class InactiveAgreement(state: String) extends ComponentError("0011", s"Agreement is in state $state")
 
 }
