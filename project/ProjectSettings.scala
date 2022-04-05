f31e45fee6f7e34635884b6bcd1638c9d757ace2
galales <25105748+galales@users.noreply.github.com>
2022-04-05 19:05:48 +0200

    pin-1346: Remove interface version

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -17,17 +17,11 @@ object ProjectSettings {
 
   private val commitSha: Option[String] = Try(Process(s"git rev-parse --short HEAD").lineStream_!.head).toOption
 
-  private val interfaceVersion: String = ComputeVersion.version match {
-    case ComputeVersion.tag(major, minor, _) => s"$major.$minor"
-    case _                                   => "0.0"
-  }
-
   //lifts some useful data in BuildInfo instance
   val buildInfoExtra: Seq[BuildInfoKey] = Seq[BuildInfoKey](
     "ciBuildNumber"    -> sys.env.get("BUILD_NUMBER"),
     "commitSha"        -> commitSha,
-    "currentBranch"    -> currentBranch,
-    "interfaceVersion" -> interfaceVersion
+    "currentBranch"    -> currentBranch
   )
 
   /** Extention methods for sbt Project instances.
