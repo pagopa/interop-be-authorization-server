231905684a88a1c1d316588236b8a5ef15526dda
galales <25105748+galales@users.noreply.github.com>
2022-04-05 16:37:50 +0200

    pin-1346: Use lazy val for configs

--- src/main/scala/it/pagopa/interop/authorizationserver/common/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/authorizationserver/common/ApplicationConfiguration.scala
@@ -7,13 +7,13 @@ object ApplicationConfiguration {
 
   lazy val config: Config = ConfigFactory.load()
 
-  def serverPort: Int = config.getInt("interop-authorization-server.port")
+  lazy val serverPort: Int = config.getInt("interop-authorization-server.port")
 
-  def authorizationManagementURL: String = config.getString("services.authorization-management")
+  lazy val authorizationManagementURL: String = config.getString("services.authorization-management")
 
-  def interopIdIssuer: String = config.getString("interop-authorization-server.issuer")
+  lazy val interopIdIssuer: String = config.getString("interop-authorization-server.issuer")
 
-  def rsaPrivatePath: String = config.getString("interop-authorization-server.rsa-private-path")
+  lazy val rsaPrivatePath: String = config.getString("interop-authorization-server.rsa-private-path")
 
   lazy val interopAudience: Set[String] =
     config.getStringList("interop-authorization-server.jwt.audience").asScala.toSet
