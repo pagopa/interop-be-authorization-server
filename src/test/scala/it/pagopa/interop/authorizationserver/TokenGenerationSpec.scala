291fe59d88d8bafcacad453802c5445ab12319d0
galales <25105748+galales@users.noreply.github.com>
2022-04-21 19:03:58 +0200

    pin-1393: Minor code cleaning

--- src/test/scala/it/pagopa/interop/authorizationserver/TokenGenerationSpec.scala
+++ src/test/scala/it/pagopa/interop/authorizationserver/TokenGenerationSpec.scala
@@ -3,19 +3,16 @@ package it.pagopa.interop.authorizationserver
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import it.pagopa.interop.authorizationserver.api.impl.AuthApiMarshallerImpl._
-import it.pagopa.interop.authorizationserver.common.ApplicationConfiguration
 import it.pagopa.interop.authorizationserver.model.ClientCredentialsResponse
-import it.pagopa.interop.authorizationserver.utils.BaseSpec
 import it.pagopa.interop.authorizationserver.utils.SpecData._
+import it.pagopa.interop.authorizationserver.utils.{BaseSpec, SpecHelper}
+import it.pagopa.interop.commons.jwt.model.{ClientAssertionChecker, ValidClientAssertionRequest}
 import it.pagopa.interop.commons.jwt.{JWTConfiguration, JWTInternalTokenConfig}
-import it.pagopa.interop.commons.jwt.model.{ClientAssertionChecker, RSA, ValidClientAssertionRequest}
-import it.pagopa.interop.commons.utils.PURPOSE_ID_CLAIM
 import org.scalatest.matchers.should.Matchers._
 
-import scala.concurrent.Future
 import scala.util.Success
 
-class TokenGenerationSpec extends BaseSpec with ScalatestRouteTest {
+class TokenGenerationSpec extends BaseSpec with SpecHelper with ScalatestRouteTest {
 
   implicit val context: Seq[(String, String)] = Seq.empty
 
@@ -32,41 +29,16 @@ class TokenGenerationSpec extends BaseSpec with ScalatestRouteTest {
 
       val resource = eServiceAudience
 
-      mockInteropTokenGenerator
-        .generateInternalToken(
-          eqTo(RSA),
-          eqTo(jwtConfig.subject),
-          eqTo(jwtConfig.audience.toList),
-          eqTo(jwtConfig.issuer),
-          eqTo(jwtConfig.durationInSeconds)
-        )
-        .returns(Success(internalToken))
-
+      mockInternalTokenGeneration(jwtConfig)
       mockClientAssertionValidator
         .extractJwtInfo(*[ValidClientAssertionRequest])
         .returns(Success(clientAssertionChecker))
 
-      mockAuthorizationManagementService
-        .getKey(eqTo(clientId), eqTo(kid))(*[Seq[(String, String)]])
-        .returns(Future.successful(clientKey))
-
+      mockKeyRetrieve()
       clientAssertionChecker.verify(*[String]).returns(Success(()))
-
-      mockAuthorizationManagementService
-        .getClient(eqTo(clientId))(*[Seq[(String, String)]])
-        .returns(Future.successful(activeClient))
-
-      mockInteropTokenGenerator
-        .generate(
-          clientAssertion = clientAssertion,
-          audience = List(eServiceAudience),
-          customClaims = Map(PURPOSE_ID_CLAIM -> purposeId.toString),
-          tokenIssuer = ApplicationConfiguration.interopIdIssuer,
-          validityDurationInSeconds = eServiceTokenDuration.toLong // TODO This could be an Int
-        )
-        .returns(Success(generatedToken))
-
-      mockQueueService.send(expectedQueueMessage).returns(Future.successful("ok"))
+      mockClientRetrieve()
+      mockTokenGeneration()
+      mockQueueMessagePublication()
 
       Get() ~> service.createToken(
         Some(clientId.toString),
---
+++ src/test/scala/it/pagopa/interop/authorizationserver/utils/SpecHelper.scala
@@ -0,0 +1,51 @@
+package it.pagopa.interop.authorizationserver.utils
+
+import it.pagopa.interop.authorizationmanagement.client.model.{Client, ClientKey}
+import it.pagopa.interop.authorizationserver.common.ApplicationConfiguration
+import it.pagopa.interop.authorizationserver.utils.SpecData._
+import it.pagopa.interop.commons.jwt.JWTInternalTokenConfig
+import it.pagopa.interop.commons.jwt.model.{RSA, Token}
+import it.pagopa.interop.commons.utils.PURPOSE_ID_CLAIM
+import org.mockito.stubbing.ScalaOngoingStubbing
+
+import scala.concurrent.Future
+import scala.util.{Success, Try}
+
+trait SpecHelper { self: BaseSpec =>
+
+  def mockInternalTokenGeneration(jwtConfig: JWTInternalTokenConfig): ScalaOngoingStubbing[Try[Token]] =
+    mockInteropTokenGenerator
+      .generateInternalToken(
+        eqTo(RSA),
+        eqTo(jwtConfig.subject),
+        eqTo(jwtConfig.audience.toList),
+        eqTo(jwtConfig.issuer),
+        eqTo(jwtConfig.durationInSeconds)
+      )
+      .returns(Success(internalToken))
+
+  def mockKeyRetrieve(): ScalaOngoingStubbing[Future[ClientKey]] =
+    mockAuthorizationManagementService
+      .getKey(eqTo(clientId), eqTo(kid))(*[Seq[(String, String)]])
+      .returns(Future.successful(clientKey))
+
+  def mockClientRetrieve(result: Client = activeClient): ScalaOngoingStubbing[Future[Client]] =
+    mockAuthorizationManagementService
+      .getClient(eqTo(clientId))(*[Seq[(String, String)]])
+      .returns(Future.successful(result))
+
+  def mockTokenGeneration(): ScalaOngoingStubbing[Try[Token]] =
+    mockInteropTokenGenerator
+      .generate(
+        clientAssertion = clientAssertion,
+        audience = List(eServiceAudience),
+        customClaims = Map(PURPOSE_ID_CLAIM -> purposeId.toString),
+        tokenIssuer = ApplicationConfiguration.interopIdIssuer,
+        validityDurationInSeconds = eServiceTokenDuration.toLong // TODO This could be an Int
+      )
+      .returns(Success(generatedToken))
+
+  def mockQueueMessagePublication(): ScalaOngoingStubbing[Future[String]] =
+    mockQueueService.send(expectedQueueMessage).returns(Future.successful("ok"))
+
+}
