aea0da6d48d75477c558343e54c9e8a546297d5e
galales <25105748+galales@users.noreply.github.com>
2022-04-22 19:03:56 +0200

    pin-1393: Remove not useful return types

--- src/test/scala/it/pagopa/interop/authorizationserver/utils/SpecHelper.scala
+++ src/test/scala/it/pagopa/interop/authorizationserver/utils/SpecHelper.scala
@@ -5,43 +5,38 @@ import it.pagopa.interop.authorizationserver.common.ApplicationConfiguration
 import it.pagopa.interop.authorizationserver.model.JWTDetailsMessage
 import it.pagopa.interop.authorizationserver.utils.SpecData._
 import it.pagopa.interop.commons.jwt.JWTInternalTokenConfig
-import it.pagopa.interop.commons.jwt.model.{JWTAlgorithmType, RSA, Token}
+import it.pagopa.interop.commons.jwt.model.{JWTAlgorithmType, RSA}
 import it.pagopa.interop.commons.utils.{ORGANIZATION_ID_CLAIM, PURPOSE_ID_CLAIM}
-import org.scalamock.handlers.{CallHandler2, CallHandler3, CallHandler5}
 import spray.json.JsonWriter
 
 import java.util.UUID
 import scala.concurrent.Future
-import scala.util.{Success, Try}
+import scala.util.Success
 
 trait SpecHelper { self: BaseSpec =>
 
-  def mockInternalTokenGeneration(
-    jwtConfig: JWTInternalTokenConfig
-  ): CallHandler5[JWTAlgorithmType, String, List[String], String, Long, Try[Token]] =
+  def mockInternalTokenGeneration(jwtConfig: JWTInternalTokenConfig) =
     (mockInteropTokenGenerator
       .generateInternalToken(_: JWTAlgorithmType, _: String, _: List[String], _: String, _: Long))
       .expects(RSA, jwtConfig.subject, jwtConfig.audience.toList, jwtConfig.issuer, jwtConfig.durationInSeconds)
       .once()
       .returns(Success(internalToken))
 
-  def mockKeyRetrieve(
-    result: ClientKey = clientKey
-  ): CallHandler3[UUID, String, Seq[(String, String)], Future[ClientKey]] =
+  def mockKeyRetrieve(result: ClientKey = clientKey) =
     (mockAuthorizationManagementService
       .getKey(_: UUID, _: String)(_: Seq[(String, String)]))
       .expects(clientId, kid, *)
       .once()
       .returns(Future.successful(result))
 
-  def mockClientRetrieve(result: Client = activeClient): CallHandler2[UUID, Seq[(String, String)], Future[Client]] =
+  def mockClientRetrieve(result: Client = activeClient) =
     (mockAuthorizationManagementService
       .getClient(_: UUID)(_: Seq[(String, String)]))
       .expects(clientId, *)
       .once()
       .returns(Future.successful(result))
 
-  def mockConsumerTokenGeneration(): CallHandler5[String, List[String], Map[String, String], String, Long, Try[Token]] =
+  def mockConsumerTokenGeneration() =
     (mockInteropTokenGenerator
       .generate(_: String, _: List[String], _: Map[String, String], _: String, _: Long))
       .expects(
@@ -54,7 +49,7 @@ trait SpecHelper { self: BaseSpec =>
       .once()
       .returns(Success(generatedToken))
 
-  def mockApiTokenGeneration(): CallHandler5[String, List[String], Map[String, String], String, Long, Try[Token]] =
+  def mockApiTokenGeneration() =
     (mockInteropTokenGenerator
       .generate(_: String, _: List[String], _: Map[String, String], _: String, _: Long))
       .expects(
@@ -67,7 +62,7 @@ trait SpecHelper { self: BaseSpec =>
       .once()
       .returns(Success(generatedToken))
 
-  def mockQueueMessagePublication(): CallHandler2[JWTDetailsMessage, JsonWriter[JWTDetailsMessage], Future[String]] =
+  def mockQueueMessagePublication() =
     (mockQueueService
       .send(_: JWTDetailsMessage)(_: JsonWriter[JWTDetailsMessage]))
       .expects(expectedQueueMessage, *)
