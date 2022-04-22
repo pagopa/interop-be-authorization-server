9832cfb1c0450f31b69adcb56517fc27e2cd22c6
galales <25105748+galales@users.noreply.github.com>
2022-04-22 18:48:13 +0200

    pin-1393: Serde auto derivation for jwt details message

--- src/main/scala/it/pagopa/interop/authorizationserver/model/JWTDetailsMessage.scala
+++ src/main/scala/it/pagopa/interop/authorizationserver/model/JWTDetailsMessage.scala
@@ -1,7 +1,7 @@
 package it.pagopa.interop.authorizationserver.model
 
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
-import spray.json.DefaultJsonProtocol._
 
 final case class JWTDetailsMessage(
   jti: String,
@@ -13,19 +13,10 @@ final case class JWTDetailsMessage(
   purposeId: Option[String],
   kid: String
 ) {
-  val readableString: String =
+  def readableString: String =
     s"jti: $jti / iat: $iat / exp: $exp / nbf: $nbf / organizationId: $organizationId / clientId: $clientId / kid: $kid / purposeId: $purposeId"
 }
 
-object JWTDetailsMessage {
-  implicit val jsonWriter: JsonWriter[JWTDetailsMessage] = new JsonWriter[JWTDetailsMessage] {
-    override def write(obj: JWTDetailsMessage): JsValue = JsObject(
-      "jti"       -> obj.jti.toJson,
-      "iat"       -> obj.iat.toJson,
-      "exp"       -> obj.exp.toJson,
-      "clientId"  -> obj.clientId.toJson,
-      "purposeId" -> obj.purposeId.toJson,
-      "kid"       -> obj.kid.toJson
-    )
-  }
+object JWTDetailsMessage extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val jwtDetailsMessageFormat: RootJsonFormat[JWTDetailsMessage] = jsonFormat8(JWTDetailsMessage.apply)
 }
