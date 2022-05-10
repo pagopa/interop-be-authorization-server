4d6cfcc1cb82794b6d0d9eb2bb9681c373449567
P3trur0 <ubaldopescatore@gmail.com>
2022-05-10 16:13:55 +0200

    renaming sqs handler instance

--- src/main/scala/it/pagopa/interop/authorizationserver/service/impl/QueueServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/authorizationserver/service/impl/QueueServiceImpl.scala
@@ -2,7 +2,7 @@ package it.pagopa.interop.authorizationserver.service.impl
 
 import it.pagopa.interop.authorizationserver.service.QueueService
 import it.pagopa.interop.commons.queue.QueueAccountInfo
-import it.pagopa.interop.commons.queue.impl.SQSSimpleWriter
+import it.pagopa.interop.commons.queue.impl.SQSSimpleHandler
 import spray.json.JsonWriter
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -10,7 +10,7 @@ import scala.concurrent.{ExecutionContext, Future}
 final case class QueueServiceImpl(queueAccountInfo: QueueAccountInfo, queueUrl: String)(implicit ec: ExecutionContext)
     extends QueueService {
 
-  val sqsWriter: SQSSimpleWriter = SQSSimpleWriter(queueAccountInfo, queueUrl)
+  val sqsHandler: SQSSimpleHandler = SQSSimpleHandler(queueAccountInfo, queueUrl)
 
-  override def send[T: JsonWriter](message: T): Future[String] = sqsWriter.send(message)
+  override def send[T: JsonWriter](message: T): Future[String] = sqsHandler.send(message)
 }
