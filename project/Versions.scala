5b6108c24f91bea4ec32998d1422589b03ebfe73
Scala Steward <scala-steward@pagopa.it>
2022-07-02 06:47:41 +0000

    Update kamon-bundle, kamon-prometheus to 2.5.5 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   lazy val catsVersion           = "2.7.0"
   lazy val jacksonVersion        = "2.11.4" // This cannot be updated yet because akka-serialization use 2.11.x version
   lazy val json4sVersion         = "4.0.4"
-  lazy val kamonVersion          = "2.5.1"
+  lazy val kamonVersion          = "2.5.5"
   lazy val logbackVersion        = "1.2.11"
   lazy val mustacheVersion       = "0.9.10"
   lazy val scalaMockVersion      = "5.2.0"
