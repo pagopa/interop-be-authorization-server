3560a4e33b822d7d8ad069ffe4bbb4437f22abe2
Antonio Gelameris <toniogela89@gmail.com>
2022-07-05 19:02:49 +0200

    Combined Scala Steward PR (#34)
    * Update sbt-tpolecat to 0.2.3 in 0.1.x
    
    * Update json4s-ext, json4s-jackson to 4.0.5 in 0.1.x
    
    * Update scalatest to 3.2.12 in 0.1.x
    
    * Update scalafmt-core to 3.5.8 in 0.1.x
    
    * Reformat with scalafmt 3.5.8
    
    * Update cats-core to 2.8.0 in 0.1.x
    
    * Update sbt-scoverage to 2.0.0 in 0.1.x
    
    * manual update
    
    Co-authored-by: Scala Steward <scala-steward@pagopa.it>

--- src/main/scala/it/pagopa/interop/authorizationserver/server/impl/Main.scala
+++ src/main/scala/it/pagopa/interop/authorizationserver/server/impl/Main.scala
@@ -13,9 +13,9 @@ import com.typesafe.scalalogging.Logger
 import scala.util.{Failure, Success}
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
-import scala.concurrent.ExecutionContext
 import buildinfo.BuildInfo
 import akka.actor.typed.DispatcherSelector
+import scala.concurrent.ExecutionContextExecutor
 
 object Main extends App with CORSSupport with Dependencies {
 
@@ -25,9 +25,9 @@ object Main extends App with CORSSupport with Dependencies {
 
   val system: ActorSystem[Nothing] = ActorSystem[Nothing](
     Behaviors.setup[Nothing] { context =>
-      implicit val actorSystem: ActorSystem[_]  = context.system
-      val selector: DispatcherSelector          = DispatcherSelector.fromConfig("futures-dispatcher")
-      implicit val blockingEc: ExecutionContext = actorSystem.dispatchers.lookup(selector)
+      implicit val actorSystem: ActorSystem[_]          = context.system
+      val selector: DispatcherSelector                  = DispatcherSelector.fromConfig("futures-dispatcher")
+      implicit val blockingEc: ExecutionContextExecutor = actorSystem.dispatchers.lookup(selector)
 
       Kamon.init()
       AkkaManagement.get(actorSystem.classicSystem).start()
--- src/main/scala/it/pagopa/interop/authorizationserver/service/impl/QueueServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/authorizationserver/service/impl/QueueServiceImpl.scala
@@ -1,14 +1,15 @@
 package it.pagopa.interop.authorizationserver.service.impl
 
 import it.pagopa.interop.authorizationserver.service.QueueService
-import it.pagopa.interop.commons.queue.impl.SQSSimpleHandler
+import it.pagopa.interop.commons.queue.impl.SQSHandler
 import spray.json.JsonWriter
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContextExecutor, Future}
 
-final case class QueueServiceImpl(queueUrl: String)(implicit blockingEc: ExecutionContext) extends QueueService {
+final case class QueueServiceImpl(queueUrl: String)(implicit blockingEc: ExecutionContextExecutor)
+    extends QueueService {
 
-  val sqsHandler: SQSSimpleHandler = SQSSimpleHandler(queueUrl)(blockingEc)
+  val sqsHandler: SQSHandler = SQSHandler(queueUrl)(blockingEc)
 
   override def send[T: JsonWriter](message: T): Future[String] = sqsHandler.send(message)
 }
