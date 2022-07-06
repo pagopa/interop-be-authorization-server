9b8c3b486132096ef54bb0e625ff9742be14f9a9
Antonio Gelameris <toniogela89@gmail.com>
2022-07-06 11:54:14 +0200

    Reverting json4s

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   lazy val akkaVersion           = "2.6.17"
   lazy val catsVersion           = "2.8.0"
   lazy val jacksonVersion        = "2.11.4" // This cannot be updated yet because akka-serialization use 2.11.x version
-  lazy val json4sVersion         = "4.0.5"
+  lazy val json4sVersion         = "4.0.4"
   lazy val kamonVersion          = "2.5.5"
   lazy val logbackVersion        = "1.2.11"
   lazy val mustacheVersion       = "0.9.10"
