c12fe670590337d4fd916503c5db801c10f42ec7
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-12-13 14:56:55 +0100

    pin-2210: Match specific errors

--- src/main/scala/it/pagopa/interop/authorizationserver/error/Handlers.scala
+++ src/main/scala/it/pagopa/interop/authorizationserver/error/Handlers.scala
@@ -2,11 +2,12 @@ package it.pagopa.interop.authorizationserver.error
 
 import akka.http.scaladsl.server.StandardRoute
 import com.typesafe.scalalogging.LoggerTakingImplicit
+import it.pagopa.interop.authorizationserver.error.AuthServerErrors._
 import it.pagopa.interop.commons.logging.ContextFieldsToLog
 import it.pagopa.interop.commons.ratelimiter
 import it.pagopa.interop.commons.ratelimiter.model.Headers
 import it.pagopa.interop.commons.utils.errors.GenericComponentErrors.TooManyRequests
-import it.pagopa.interop.commons.utils.errors.{AkkaResponses, ComponentError, ServiceCode}
+import it.pagopa.interop.commons.utils.errors.{AkkaResponses, ServiceCode}
 
 import scala.util.{Failure, Try}
 
@@ -18,7 +19,15 @@ object Handlers extends AkkaResponses {
     contexts: Seq[(String, String)],
     logger: LoggerTakingImplicit[ContextFieldsToLog]
   ): PartialFunction[Try[_], StandardRoute] = {
-    case Failure(ex: ComponentError)                           => badRequest(ex, logMessage)
+    case Failure(ex: PurposeNotFound)                          => badRequest(ex, logMessage)
+    case Failure(ex: InactiveClient)                           => badRequest(ex, logMessage)
+    case Failure(ex: InactivePurpose)                          => badRequest(ex, logMessage)
+    case Failure(ex: InactiveEService)                         => badRequest(ex, logMessage)
+    case Failure(ex: InactiveAgreement)                        => badRequest(ex, logMessage)
+    case Failure(ex: PurposeIdNotProvided.type)                => badRequest(ex, logMessage)
+    case Failure(ex: InvalidAssertion)                         => badRequest(ex, logMessage)
+    case Failure(ex: KeyNotFound)                              => badRequest(ex, logMessage)
+    case Failure(ex: InvalidAssertionSignature)                => badRequest(ex, logMessage)
     case Failure(ex: ratelimiter.error.Errors.TooManyRequests) =>
       tooManyRequests(TooManyRequests, logMessage, Headers.headersFromStatus(ex.status))
     case Failure(ex)                                           => internalServerError(ex, logMessage)
