e3f97b78453fda57f8c3667dca8c6fffc7568eea
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-05-30 18:25:02 +0200

    pin-2541: Minor code cleaning

--- client-assertion-validation/src/test/scala/it/pagopa/interop/clientassertionvalidation/SpecUtil.scala
+++ client-assertion-validation/src/test/scala/it/pagopa/interop/clientassertionvalidation/SpecUtil.scala
@@ -2,23 +2,6 @@ package it.pagopa.interop.clientassertionvalidation
 
 object SpecUtil {
 
-//  val successfulJwtValidator: ClientAssertionValidator = new DefaultClientAssertionValidator with PublicKeysHolder {
-//    var publicKeyset: Map[KID, SerializedKey]                                        = Map.empty
-//    override protected val claimsVerifier: DefaultJWTClaimsVerifier[SecurityContext] =
-//      getClaimsVerifier(audience = Set(SpecData.clientAssertionAudience))
-//  }
-//
-//  val failureJwtValidator: ClientAssertionValidator = new DefaultClientAssertionValidator with PublicKeysHolder {
-//    var publicKeyset: Map[KID, SerializedKey]                                        = Map.empty
-//    override protected val claimsVerifier: DefaultJWTClaimsVerifier[SecurityContext] =
-//      getClaimsVerifier(audience = Set("another-audience"))
-//  }
-//
-//  val validChecker: ClientAssertionChecker =
-//    validateClientAssertion(Some(clientId.toString), validClientAssertion, clientAssertionType, grantType)(
-//      successfulJwtValidator
-//    ).getOrElse(fail("Error generating checker in test"))
-
   val successfulJwtValidator: ClientAssertionValidator =
     new NimbusClientAssertionValidator(Set(SpecData.clientAssertionAudience))
 
